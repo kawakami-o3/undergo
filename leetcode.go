d281201aa0976910067dbb8a093f2968667732de
kawakami <kawakami.ozone@gmail.com>
2023-05-21 14:31:08 +0000

    .

--- leetcode.go
+++ leetcode.go
@@ -73,3 +73,68 @@ func Ints2d(s string) [][]int {
 
 	return slice2d
 }
+
+func Floats(s string) []float64 {
+	// s = '[1,2,3,...]'
+	slice := []float64{}
+	if len(s) < 2 {
+		return slice
+	}
+	s = s[1:len(s)-1] + string(',')
+	var digits strings.Builder
+	for _, c := range s {
+
+		switch c {
+		case ' ':
+			continue
+		case ',':
+			i, _ := strconv.ParseFloat(digits.String(), 64)
+			slice = append(slice, i)
+			digits.Reset()
+		default:
+			digits.WriteRune(c)
+		}
+	}
+	return slice
+}
+
+func Floats2d(s string) [][]float64 {
+	// s = '[[1,2,3], [4,5,6], ....]'
+	slice2d := [][]float64{}
+	if len(s) < 2 {
+		return slice2d
+	}
+
+	s = s[1 : len(s)-1]
+	slice := []float64{}
+	var digits strings.Builder
+	nest := 0
+	for _, c := range s {
+		switch c {
+		case ' ':
+			continue
+		case ',':
+			if nest == 0 {
+				continue
+			}
+			i, _ := strconv.ParseFloat(digits.String(), 64)
+			slice = append(slice, i)
+			digits.Reset()
+		case '[':
+			nest++
+			slice = []float64{}
+		case ']':
+			nest--
+			if digits.Len() > 0 {
+				i, _ := strconv.ParseFloat(digits.String(), 64)
+				slice = append(slice, i)
+				digits.Reset()
+			}
+			slice2d = append(slice2d, slice)
+		default:
+			digits.WriteRune(c)
+		}
+	}
+
+	return slice2d
+}
--- leetcode_test.go
+++ leetcode_test.go
@@ -7,14 +7,18 @@ import (
 )
 
 func TestInts(t *testing.T) {
-
 	assert.EqualValues(t, []int{1, 2, 3}, Ints("[1, 2, 3]"), "")
-
 }
 
 func TestInts2d(t *testing.T) {
+	assert.EqualValues(t, [][]int{{1, 2}, {3}}, Ints2d("[[1, 2], [3]]"), "")
+	assert.EqualValues(t, [][]int{{}, {}}, Ints2d("[[], []]"), "")
+}
 
-	assert.EqualValues(t, [][]int{[]int{1, 2}, []int{3}}, Ints2d("[[1, 2], [3]]"), "")
-	assert.EqualValues(t, [][]int{[]int{}, []int{}}, Ints2d("[[], []]"), "")
+func TestFloats(t *testing.T) {
+	assert.EqualValues(t, []float64{1.0, 2.0, 3.0}, Floats("[1.00, 2.0, 3.0]"), "")
+}
 
+func TestFloats2d(t *testing.T) {
+	assert.EqualValues(t, [][]float64{{1.0, 2.0}, {3.0}}, Floats2d("[[1.0, 2], [3.00]]"), "")
 }
